a2ce1b57de31c62acab6f51a042809018c2f8fcf
Ekaterina Ryabova <remboinc@mail.ru>
2022-10-23 00:25:15 +0300

    publish photo to Telegram channel.

--- telegram_space_bot.py
+++ telegram_space_bot.py
@@ -8,7 +8,9 @@ def bot_send_a_massage():
     bot = telegram.Bot(token)
     chat_id = '-1001650514384'
     text = input('Кать, введи сообщение')
+    image = '/Users/ok_user/PycharmProjects/untitled3/images/space_image_0.jpg'
     bot.send_message(chat_id=chat_id, text=text)
+    bot.send_document(chat_id=chat_id, document=open(image, 'rb'))
 
 
 def main():
