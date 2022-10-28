9f42f574999b624648e40158aadc0e5138e548ce
Ekaterina Ryabova <remboinc@mail.ru>
2022-10-28 22:01:29 +0300

    Removed redundant variables

--- get_epic_image_links.py
+++ get_epic_image_links.py
@@ -16,11 +16,8 @@ def get_epic_image_links(nasa_token):
         just_date = date_and_time['date'].split()[0]
         datetime = date.fromisoformat(just_date).timetuple()
         (tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec, tm_wday, tm_yday, tm_isdst) = datetime
-        year = tm_year
-        month = tm_mon
-        day = tm_mday
         name_of_image = date_and_time['image']
-        url = f'https://epic.gsfc.nasa.gov/archive/natural/{year}/{month}/{day}/png/{name_of_image}.png'
+        url = f'https://epic.gsfc.nasa.gov/archive/natural/{tm_year}/{tm_mon}/{tm_mday}/png/{name_of_image}.png'
         epic_images.append(url)
     return epic_images
 
