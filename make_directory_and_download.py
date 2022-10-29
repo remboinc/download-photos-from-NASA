e8c38c899f9bb4cf900347ce031cacef5f0309f7
Ekaterina Ryabova <remboinc@mail.ru>
2022-10-29 15:19:28 +0300

    Variable name changed

--- make_directory_and_download.py
+++ make_directory_and_download.py
@@ -5,12 +5,12 @@ import requests
 
 def download_images(all_image_links, safe_folder):
     os.makedirs(safe_folder, exist_ok=True)
-    for index, space_image in enumerate(all_image_links):
-        path = urlparse(space_image).path
+    for index, space_image_link in enumerate(all_image_links):
+        path = urlparse(space_image_link).path
         extension = os.path.splitext(path)[1]
         image_name = f'space_image_{index}{extension}'
         filename = os.path.join(safe_folder, image_name)
-        response = requests.get(space_image)
+        response = requests.get(space_image_link)
         response.raise_for_status()
         with open(filename, 'wb') as file:
             file.write(response.content)
